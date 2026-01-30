db292e19dda8ac0dbcf8b75720a4a742e6ce47c5
CHAN HYUK JUNG <chanhyukjung@ibm.com>
2026-01-30 17:41:13 +0900

    배경 변경

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -11,7 +11,7 @@ function Main() {
     <div className='main center'>
       <div className='add_windows_wrap invers_center'>
           <div className='add_windows center'>
-            <Plus color='white' className='add_windows_button' />
+            <Plus color='white'  className='add_windows_button' />
           </div>
           <p>클릭하여 시작하기</p>
       </div>
